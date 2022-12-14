58b86467f67da9da6e28ba688927b5b0ccf42553
Artem <ckeletoh@yandex.ru>
2022-12-15 01:19:58 +0600

    Updates

---
+++ precache-manifest.88ad9be58d702225aef9de21cab014aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12581d3b307a32150091",
+    "url": "/static/css/main.5829dd6c.chunk.css"
+  },
+  {
+    "revision": "12581d3b307a32150091",
+    "url": "/static/js/main.12581d3b.chunk.js"
+  },
+  {
+    "revision": "2f73aeaaf2872a6f020b",
+    "url": "/static/css/1.bcca8d36.chunk.css"
+  },
+  {
+    "revision": "2f73aeaaf2872a6f020b",
+    "url": "/static/js/1.2f73aeaa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c12226bbccafe3d64d7de658e4667cd0",
+    "url": "/static/media/kit_meme.c12226bb.jpg"
+  },
+  {
+    "revision": "5b833ba4a3baefe0a406112c211ae9b0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sgs-test-app/precache-manifest.1aed14db3e27a345d21c8efb67869af4.js"
+  "/precache-manifest.88ad9be58d702225aef9de21cab014aa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/sgs-test-app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
