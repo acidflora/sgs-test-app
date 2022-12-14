dd09c9fe0f42b65da6228068dddf691a11aa4ec3
Artem <ckeletoh@yandex.ru>
2022-12-14 23:19:29 +0600

    Updates

---
+++ precache-manifest.f44bec12ec1a15c229b1b6e4d97993e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc0ac29e7741cb5a722e",
+    "url": "/sgs-test-app/static/css/main.5829dd6c.chunk.css"
+  },
+  {
+    "revision": "cc0ac29e7741cb5a722e",
+    "url": "/sgs-test-app/static/js/main.cc0ac29e.chunk.js"
+  },
+  {
+    "revision": "c639152ec4e38a5e3394",
+    "url": "/sgs-test-app/static/css/1.bcca8d36.chunk.css"
+  },
+  {
+    "revision": "c639152ec4e38a5e3394",
+    "url": "/sgs-test-app/static/js/1.c639152e.chunk.js"
+  },
+  {
+    "revision": "7f30167eea2060e31c11",
+    "url": "/sgs-test-app/static/js/runtime~main.7f30167e.js"
+  },
+  {
+    "revision": "c12226bbccafe3d64d7de658e4667cd0",
+    "url": "/sgs-test-app/static/media/kit_meme.c12226bb.jpg"
+  },
+  {
+    "revision": "b03c0e8994567e666bbe4dcbfdbd1ed2",
+    "url": "/sgs-test-app/index.html"
+  }
+];
